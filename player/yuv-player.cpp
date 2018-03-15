4fb1710be69e90c6be372d9373e30575126aa9df
moony <sangmoon.lee@titanplatform.net>
2018-03-15 18:32:20 +0900

    shader를 실행하기 위한 기본 기능 추가 vertex buffer 를 이용해 영상에 해당하는 부분을 사각형으로 보여줌

--- player/yuv-player.cpp
+++ player/yuv-player.cpp
@@ -5,6 +5,8 @@
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
 
+#include <shader.hpp>
+
 int main()
 {
 	// Initialise GLFW
@@ -22,8 +24,8 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
 
 																   // Open a window and create its OpenGL context
-	window = glfwCreateWindow(1024, 768, "YUV-player", NULL, NULL);
-	if (window == NULL) {
+	window = glfwCreateWindow(1024, 768, "YUV-player", nullptr, nullptr);
+	if (window == nullptr) {
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		getchar();
 		glfwTerminate();
@@ -40,11 +42,67 @@ int main()
 		return -1;
 	}
 
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("videoCoordLoading.vs", "colorSpaceConversion.fs");
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// positions          // texture coords
+		0.5f,  0.5f, 0.0f,     1.0f, 0.0f,		// top right
+		0.5f, -0.5f, 0.0f,     1.0f, 1.0f,		// bottom right
+		-0.5f, -0.5f, 0.0f,    0.0f, 1.0f,		// bottom left
+		-0.5f,  0.5f, 0.0f,    0.0f, 0.0f		// top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
 	do {
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 
-	glfwTerminate();
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
+	glfwTerminate();
 	return 0;
 }

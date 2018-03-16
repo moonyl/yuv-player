eb598601e970029a9029f5bf54e12a92844fc1c2
moony <sangmoon.lee@titanplatform.net>
2018-03-16 15:03:52 +0900

    yuv texture loading 기능 추가 입력으로 사용할 yuv 영상 추가

--- player/colorSpaceConversion.fs
+++ player/colorSpaceConversion.fs
@@ -1,46 +1,28 @@
 #version 330 core
 out vec4 FragColor;
-  
-in vec3 ourColor;
+
 in vec2 TexCoord;
 
-//uniform sampler2D ourTexture;
-uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform sampler2D texture3;
-uniform sampler2D texture4;
-uniform sampler2D texture5;
+uniform sampler2D textureY;
+uniform sampler2D textureU;
+uniform sampler2D textureV;
+
 
 void main()
 {
-	//vec3 yuv;
-	//vec3 rgb;
-	//yuv.x = texture(texture3, TexCoord).r;
-	//yuv.y = texture(texture4, TexCoord).r - 0.5;
-	//yuv.z = texture(texture5, TexCoord).r - 0.5;
-	//rgb = mat3( 1,       1,         1,
-    //          0,       -0.39465,  2.03211,
-    //          1.13983, -0.58060,  0) * yuv;   
-
 	mediump float r,g,b,y,u,v;
-	y=texture(texture3, TexCoord).r;
-	u=texture(texture4, TexCoord).r;
-	v=texture(texture5, TexCoord).r;
+	y=texture(textureY, TexCoord).r;
+	u=texture(textureU, TexCoord).r;
+	v=texture(textureV, TexCoord).r;
 
 	y=1.1643*(y-0.0625);
 	u=u-0.5;
 	v=v-0.5;
-	
+
 	r=y+1.5958*v ;
 	g=y-0.39173*u-0.81290*v;
 	b=y+2.017*u;
-	FragColor = vec4(r, g, b, 1.0);
-	//FragColor = vec4(u, 0, 0, 1.0);
 
-	//FragColor = vec4(rgb, 1);
-    //FragColor = texture(ourTexture, TexCoord);
-	//FragColor = texture(texture1, TexCoord);
-	//FragColor = texture(texture3, TexCoord);
-	//FragColor = mix(texture(ourTexture, TexCoord), texture(texture2, TexCoord), 0.5);
-	//FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+	FragColor = vec4(r, g, b, 1.0);
+
 }
--- player/yuv-player.cpp
+++ player/yuv-player.cpp
@@ -1,11 +1,29 @@
 
 #include <cstdio>
+#include <iostream>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
 
 #include <shader.hpp>
+
+std::FILE *infile = nullptr;
+enum {
+	pixel_w = 256,
+	pixel_h = 256
+};
+uint8_t buf[pixel_w*pixel_h * 3 / 2];
+uint8_t *plane[3];
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
 
 int main()
 {
@@ -32,6 +50,7 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	// Initialize GLEW
 	glewExperimental = true; // Needed for core profile
@@ -45,6 +64,24 @@ int main()
 	// Create and compile our GLSL program from the shaders
 	GLuint programID = LoadShaders("videoCoordLoading.vs", "colorSpaceConversion.fs");
 
+	if ((infile = std::fopen("seol.yuv", "rb")) == NULL) {
+		//if ((infile = std::fopen("seol.yuv", "rb")) == NULL) {
+		std::cerr << "cannot open this file\n" << std::endl;
+	}
+
+	plane[0] = buf;
+	plane[1] = plane[0] + pixel_w * pixel_h;
+	plane[2] = plane[1] + pixel_w * pixel_h / 4;
+
+	std::cout << "width height " << pixel_w << " " << pixel_h << std::endl;
+
+	if (std::fread(buf, 1, pixel_w*pixel_h * 3 / 2, infile) != pixel_w * pixel_h * 3 / 2) {
+		// Loop  
+		std::cerr << "read byte not enough. " << std::endl;
+		std::fseek(infile, 0, 0);
+		std::fread(buf, 1, pixel_w*pixel_h * 3 / 2, infile);
+	}
+
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
@@ -54,6 +91,7 @@ int main()
 		-0.5f, -0.5f, 0.0f,    0.0f, 1.0f,		// bottom left
 		-0.5f,  0.5f, 0.0f,    0.0f, 0.0f		// top left 
 	};
+
 	unsigned int indices[] = {
 		0, 1, 3, // first triangle
 		1, 2, 3  // second triangle
@@ -80,12 +118,57 @@ int main()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
+	unsigned int textureY, textureU, textureV;
+	glGenTextures(1, &textureY);
+	glBindTexture(GL_TEXTURE_2D, textureY);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+	// set texture filtering parameters
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, pixel_w, pixel_h, 0, GL_RED, GL_UNSIGNED_BYTE, plane[0]);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glGenTextures(1, &textureU);
+	glBindTexture(GL_TEXTURE_2D, textureU);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+	
+	// set texture filtering parameters
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, pixel_w / 2, pixel_h / 2, 0, GL_RED, GL_UNSIGNED_BYTE, plane[1]);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glGenTextures(1, &textureV);
+	glBindTexture(GL_TEXTURE_2D, textureV);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+	// set texture filtering parameters
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, pixel_w / 2, pixel_h / 2, 0, GL_RED, GL_UNSIGNED_BYTE, plane[2]);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glUseProgram(programID);
+
+	glUniform1i(glGetUniformLocation(programID, "textureY"), 0);
+	glUniform1i(glGetUniformLocation(programID, "textureU"), 1);
+	glUniform1i(glGetUniformLocation(programID, "textureV"), 2);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
 	do {
 		// render
 		// ------
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureY);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, textureU);
+
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, textureV);
 
 		// Use our shader
 		glUseProgram(programID);
@@ -94,6 +177,7 @@ int main()
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
 	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 
@@ -106,3 +190,4 @@ int main()
 	glfwTerminate();
 	return 0;
 }
+
